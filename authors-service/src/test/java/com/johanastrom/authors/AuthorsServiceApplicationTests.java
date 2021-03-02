8d61299a7b9b5e8aa1f640e99342929cca35893a
Johan Astrom <johan.astrom89@gmail.com>
2021-03-02 16:32:02 +0100

    Tests

--- authors-service/src/test/java/com/johanastrom/authors/AuthorsServiceApplicationTests.java
+++ authors-service/src/test/java/com/johanastrom/authors/AuthorsServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.johanastrom.authors;
 
+import com.johanastrom.authors.dtos.AuthorRecord;
 import com.johanastrom.authors.entities.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.sql.Timestamp;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -21,9 +26,17 @@ class AuthorsServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        var result = testClient.getForEntity("http://localhost:" + port + "/authors", Author[].class);
-        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
-        //assertThat(result.getBody().length).isGreaterThan(0);
+        var postResult = testClient.postForEntity("http://localhost:" + port + "/authors", new AuthorRecord(1, "test", "test", new Timestamp(1000)), AuthorRecord.class);
+        assertThat(postResult.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        var getResult = testClient.getForEntity("http://localhost:" + port + "/authors", Author[].class);
+        assertThat(getResult.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(getResult.getBody().length).isGreaterThan(0);
+
+        var patchedAuthor = testClient.patchForObject("http://localhost:" + port + "/authors", new AuthorRecord(1, "test2", "test2", new Timestamp(1000)), AuthorRecord.class);
+        ResponseEntity<AuthorRecord> patchResult = ResponseEntity.of(Optional.of(patchedAuthor));
+        assertThat(patchResult.getStatusCode().equals(HttpStatus.OK));
+
     }
 
 }
--- authors-service/src/test/java/com/johanastrom/authors/controllers/MvcTest.java
+++ authors-service/src/test/java/com/johanastrom/authors/controllers/MvcTest.java
@@ -1,5 +1,6 @@
 package com.johanastrom.authors.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.johanastrom.authors.configurations.JohansConfig;
 import com.johanastrom.authors.dtos.AuthorRecord;
 import com.johanastrom.authors.services.IntermediaryService;
@@ -10,12 +11,17 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
+import org.hamcrest.Matchers.*;
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class MvcTest {
@@ -29,8 +35,11 @@ public class MvcTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    public void callingWithURLPersonsPlusIDShouldReturnJSONWithOnePersonObject() throws Exception {
+    public void callingWithURLAuhtorsPlusIDShouldReturnJSONWithOneAuthorRecord() throws Exception {
         when(service.getOneAuthor(1)).thenReturn(Optional.of(
                 new AuthorRecord(1, "test", "test", new Timestamp(1000))));
         var result = mockMvc.perform(MockMvcRequestBuilders.get("/authors/1")
@@ -38,6 +47,60 @@ public class MvcTest {
                 .andReturn();
         assertThat(result.getResponse().getStatus()).isEqualTo(200);
         assertThat(result.getResponse().getContentType()).isEqualTo("application/json");
+    }
+
+    @Test
+    public void callingWithURLAuthorsShouldReturnJSONListOfAuthorRecords() throws Exception{
+        when(service.getAllAuthors()).thenReturn(List.of(
+                new AuthorRecord(1, "Test", "Test", new Timestamp(1000))));
+        var result=mockMvc.perform(MockMvcRequestBuilders.get("/authors")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        assertThat(result.getResponse().getContentType()).isEqualTo("application/json");
+    }
+
+    @Test
+    public void callingWithURLAuthorsPlusSearchAndURLParametersShouldReturnJSONListOfAuthorRecords() throws Exception{
+        when(service.getByLastName("Test")).thenReturn(List.of(
+                new AuthorRecord(1, "Test", "Test", new Timestamp(1000))));
+        var result=mockMvc.perform(MockMvcRequestBuilders.get("/authors/search?lastName=Test")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        assertThat(result.getResponse().getContentType()).isEqualTo("application/json");
+    }
+
+    @Test
+    public void postRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception{
+        var author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
+        when(service.createAuthor(any(AuthorRecord.class)))
+                .thenReturn(Optional.of(author));
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/authors")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(author)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is("Test")))
+                .andExpect(jsonPath("$.id", is(1)));
+
+    }
+
+    // TODO: 2021-03-02 Lös denna
+    @Test
+    public void putRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception{
+        var author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
+        when(service.replace(1, any(AuthorRecord.class)))
+                .thenReturn(Optional.of(author));
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/authors")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(author)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is("Test")))
+                .andExpect(jsonPath("$.id", is(1)));
 
     }
 }
