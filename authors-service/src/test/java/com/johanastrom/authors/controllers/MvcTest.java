4d2c208e531acad15cc877a7a1299539f1444f50
Johan Astrom <johan.astrom89@gmail.com>
2021-03-03 08:05:22 +0100

    Added authors mvctests

--- authors-service/src/test/java/com/johanastrom/authors/controllers/MvcTest.java
+++ authors-service/src/test/java/com/johanastrom/authors/controllers/MvcTest.java
@@ -12,17 +12,20 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.hamcrest.Matchers.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class MvcTest {
@@ -51,10 +54,10 @@ public class MvcTest {
     }
 
     @Test
-    public void callingWithURLAuthorsShouldReturnJSONListOfAuthorRecords() throws Exception{
+    public void callingWithURLAuthorsShouldReturnJSONListOfAuthorRecords() throws Exception {
         when(service.getAllAuthors()).thenReturn(List.of(
                 new AuthorRecord(1, "Test", "Test", new Timestamp(1000))));
-        var result=mockMvc.perform(MockMvcRequestBuilders.get("/authors")
+        var result = mockMvc.perform(MockMvcRequestBuilders.get("/authors")
                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn();
         assertThat(result.getResponse().getStatus()).isEqualTo(200);
@@ -62,10 +65,10 @@ public class MvcTest {
     }
 
     @Test
-    public void callingWithURLAuthorsPlusSearchAndURLParametersShouldReturnJSONListOfAuthorRecords() throws Exception{
+    public void callingWithURLAuthorsPlusSearchAndURLParametersShouldReturnJSONListOfAuthorRecords() throws Exception {
         when(service.getByLastName("Test")).thenReturn(List.of(
                 new AuthorRecord(1, "Test", "Test", new Timestamp(1000))));
-        var result=mockMvc.perform(MockMvcRequestBuilders.get("/authors")
+        var result = mockMvc.perform(MockMvcRequestBuilders.get("/authors")
                 .param("lastName", "Test")
                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn();
@@ -74,7 +77,7 @@ public class MvcTest {
     }
 
     @Test
-    public void postRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception{
+    public void postRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception {
         var author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
         when(service.createAuthor(any(AuthorRecord.class)))
                 .thenReturn(Optional.of(author));
@@ -90,7 +93,7 @@ public class MvcTest {
     }
 
     @Test
-    public void putRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception{
+    public void putRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception {
         var author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
         when(service.replace(any(Integer.class), any(AuthorRecord.class)))
                 .thenReturn(Optional.of(author));
@@ -105,7 +108,7 @@ public class MvcTest {
     }
 
     @Test
-    public void patchRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception{
+    public void patchRequestShouldGenerateJSONOfAuthorRecordObject() throws Exception {
         var author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
         var authorData = new AuthorPersonalData("Test", "Test", new Timestamp(1000));
         when(service.update(any(Integer.class), any(AuthorPersonalData.class)))
@@ -121,12 +124,61 @@ public class MvcTest {
     }
 
     @Test
-    public void deleteRequestShouldGenerateHttpStatusOK() throws Exception{
+    public void deleteRequestShouldGenerateHttpStatusOK() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/authors/{id}", "1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void callingGetWithInvalidIdShouldGenerateHttpStatusNotFound() throws Exception {
+        when(service.getOneAuthor(any(Integer.class))).thenReturn(Optional.empty());
+        mockMvc.perform(MockMvcRequestBuilders.get("/authors/{id}", "2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void callingPutWithInvalidIdShouldGenerateHttpStatusNotFound() throws Exception {
+        AuthorRecord author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
+        when(service.replace(any(Integer.class), any(AuthorRecord.class))).thenReturn(Optional.empty());
+        mockMvc.perform(MockMvcRequestBuilders.put("/authors/{id}", "2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(author)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void callingPatchWithInvalidIdShouldGenerateHttpStatusNotFound() throws Exception {
+        AuthorRecord author = new AuthorRecord(1, "Test", "Test", new Timestamp(1000));
+        when(service.update(any(Integer.class), any(AuthorPersonalData.class))).thenReturn(Optional.empty());
+        mockMvc.perform(MockMvcRequestBuilders.patch("/authors/{id}", "2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(author)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
+                .andExpect(status().isNotFound());
+    }
+
+    // TODO: 2021-03-03 Färdigställ
+    @Test
+    public void callingDeleteWithInvalidIdShouldGenerateHttpStatusNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/authors/{id}", "2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
+                .andExpect(status().isNotFound());
+    }
+
 
 }
+
+
+
+
+
